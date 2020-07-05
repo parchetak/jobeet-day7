0ceaf00cbd6c7b6adc98a6bb33546f0c308f6393
parchetak <parchetak@outlook.com>
2020-07-05 11:30:22 +0100

    day-7

---
+++ src/Repository/JobRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Category;
+use App\Entity\Job;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+
+class JobRepository extends EntityRepository
+{
+    /**
+     * @param int|null $categoryId
+     *
+     * @return Job[]
+     */
+    public function findActiveJobs(int $categoryId = null)
+    {
+        $qb = $this->createQueryBuilder('j')
+            ->where('j.expiresAt > :date')
+            ->setParameter('date', new \DateTime())
+            ->orderBy('j.expiresAt', 'DESC');
+
+        if ($categoryId) {
+            $qb->andWhere('j.category = :categoryId')
+                ->setParameter('categoryId', $categoryId);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param int $id
+     *
+     * @throws NonUniqueResultException
+     *
+     * @return Job|null
+     */
+    public function findActiveJob(int $id) : ?Job
+    {
+        return $this->createQueryBuilder('j')
+            ->where('j.id = :id')
+            ->andWhere('j.expiresAt > :date')
+            ->setParameter('id', $id)
+            ->setParameter('date', new \DateTime())
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @param Category $category
+     * @return AbstractQuery
+     */
+    public function getPaginatedActiveJobsByCategoryQuery(Category $category) : AbstractQuery
+    {
+        return $this->createQueryBuilder('j')
+            ->where('j.category = :category')
+            ->andWhere('j.expiresAt > :date')
+            ->setParameter('category', $category)
+            ->setParameter('date', new \DateTime())
+            ->getQuery();
+    }
+}
